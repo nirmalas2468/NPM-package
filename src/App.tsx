bd546030fd33e2a80944e0803d76b11ad94b67a0
muthuannamalai12 <muthuannamalai2002@gmail.com>
2025-03-19 16:57:05 +0530

    Added the code to monitor the browser using the browser agent from new relic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent'
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
+import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Populate using values in copy-paste JavaScript snippet.
+const options = {
+    init: {session_replay:{enabled:true,block_selector:'',mask_text_selector:'*',sampling_rate:10.0,error_sampling_rate:100.0,mask_all_inputs:true,collect_fonts:true,inline_images:false,inline_stylesheet:true,fix_stylesheets:true,mask_input_options:{}},distributed_tracing:{enabled:true},privacy:{cookies_enabled:true},ajax:{deny_list:["bam.nr-data.net"]}}, // NREUM.init
+    info: {beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"NRBR-831606d506bdf5cdc0b",applicationID:"601539091",sa:1}, // NREUM.info
+    loader_config: {accountID:"1",trustKey:"1",agentID:"601539091",licenseKey:"NRBR-831606d506bdf5cdc0b",applicationID:"601539091"} // NREUM.loader_config
+  }
+  
+  // The agent loader code executes immediately on instantiation.
+  new BrowserAgent(options)
+
 createRoot(document.getElementById("root")!).render(<App />);
+
