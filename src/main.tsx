0d53329069ce85f0dfee4530667794d5f1be960a
muthuannamalai12 <muthuannamalai2002@gmail.com>
2025-08-19 19:47:28 +0530

    Added setUserId code

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { BrowserAgent } from '@newrelic/browser-agent/loaders/browser-agent'
+import { Metrics } from '@newrelic/browser-agent/features/metrics'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
@@ -7,11 +8,24 @@ import './index.css'
 const options = {
     init: {session_replay:{enabled:true,block_selector:'',mask_text_selector:'*',sampling_rate:10.0,error_sampling_rate:100.0,mask_all_inputs:true,collect_fonts:true,inline_images:false,inline_stylesheet:true,fix_stylesheets:true,mask_input_options:{}},distributed_tracing:{enabled:true},privacy:{cookies_enabled:true},ajax:{deny_list:["bam.nr-data.net"]}}, // NREUM.init
     info: {beacon:"bam.nr-data.net",errorBeacon:"bam.nr-data.net",licenseKey:"NRBR-831606d506bdf5cdc0b",applicationID:"601539290",sa:1}, // NREUM.info
-    loader_config: {accountID:"1",trustKey:"1",agentID:"601539290",licenseKey:"NRBR-831606d506bdf5cdc0b",applicationID:"601539290"} // NREUM.loader_config
+    loader_config: {accountID:"1",trustKey:"1",agentID:"601539290",licenseKey:"NRBR-831606d506bdf5cdc0b",applicationID:"601539290"}, // NREUM.loader_config
+    features: [
+      Metrics
+    ]
   }
   
-  // The agent loader code executes immediately on instantiation.
-  new BrowserAgent(options)
+  // Before settin setUserId
+  // new BrowserAgent(options)
+
+  // Modification to add the userId
+  const browserAgent = new BrowserAgent({
+  ...options,
+  features: [
+    Metrics
+  ]
+})
+
+browserAgent.setUserId('user-1234-v1.0')
 
 createRoot(document.getElementById("root")!).render(<App />);
 
